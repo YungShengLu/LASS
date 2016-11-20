043d58d36c7ce3ca2eb72b408100bfec73bd4a88
balilarder <balilarder@yahoo.com.tw>
2016-11-20 16:51:08 +0800

    first commit: region graph

---
+++ airbox_parser/region_graph.py
@@ -0,0 +1,11 @@
+# The program will query data in the DB, output a graph with nodes and links.
+
+
+# example(bigger nodes):
+
+# Taipei: {<id1>, <id2>...}
+# Kaohsiung: {<id3>, <id4>}
+
+# example(smaller node, with distance):
+# {id1: {id2:13, id3:16, id4:18}}
+# {id2: {id1:13, id6:19}}....
