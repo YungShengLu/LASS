989edfaaa9e02e5256c01a0c2c7d2a9dcf2a5992
YungShengLu <jones830802@gmail.com>
2016-11-27 18:35:52 +0800

    Update - v1.2

--- airbox_parser/airbox/parseSite.py
+++ airbox_parser/airbox/parseSite.py
@@ -1,78 +1,59 @@
 from requests import get, exceptions
-from progressbar import ProgressBar, Bar, Percentage
-from time import sleep
+from time import sleep, gmtime, strftime
 from influxdb import InfluxDBClient
-import sys
 
 
 class parseSite:
-	def __init__(self, city, siteID, database):
-		self.parseCity = city
-		self.parseSite = siteID
-		self.url = 'http://nrl.iis.sinica.edu.tw/LASS/last.php?device_id='
+	def __init__(self, src, database):
+		self.src = src
+		self.url = 'http://nrl.iis.sinica.edu.tw/LASS/last-all-' + self.src + '.json'
 		self.jsonData = {}
-		#connect with DB (AirBox / AirBox_test)
+
+		#connect with DB (AirBox / LASS)
 		self.database = database
 		self.client = InfluxDBClient('localhost', 8086, 'root', 'root', self.database)
 		self.client.create_database(database)
 
 	def parseData(self):
-		count = 12	#test
+		try:
+			req = get(self.url, timeout = 25)
+		except exceptions.Timeout as e:
+			print('Website cannot connect: ' + self.url)
 
-		print('>>> Parse site data: ' + self.parseCity.capitalize())
-		progress = ProgressBar(maxval = self.parseSite['siteNum'], widgets = [Bar('=', '[', ']'), ' ', Percentage()]).start()
-		self.jsonData = {}
+		#parse data
+		self.jsonData = req.json()
 
-		for ID, perc, i in zip(self.parseSite['id'], range(self.parseSite['siteNum']), range(count)):
-			try:
-				req = get(self.url + ID, timeout = 25)
-			except exceptions.Timeout as e:
-				print('Site cannot connect: ' + ID)
-				continue
-
-			self.jsonData = req.json()
-
-			'''if self.jsonData.get('app') is None:
-				self.jsonData.update({'app': 'N/A'})
-			if self.jsonData.get('gps_lat') is None:
-				self.jsonData.update({'gps_lat': -1.0})
-			if self.jsonData.get('gps_lon') is None:
-				self.jsonData.update({'gps_lon': -1.0})
-			if self.jsonData.get('gps_num') is None:
-				self.jsonData.update({'gps_num': -1})
-			if self.jsonData.get('s_t0') is None:
-				self.jsonData.update({'s_t0' : -1.0})
-			if self.jsonData.get('s_h0') is None:
-				self.jsonData.update({'s_h0': -1})
-			if self.jsonData.get('s_d0') is None:
-				self.jsonData.update({'s_d0': -1})
-			if self.jsonData.get('s_d1') is None:
-				self.jsonData.update({'s_d1': -1})
-			if self.jsonData.get('s_d2') is None:
-				self.jsonData.update({'s_d2': -1})'''
-			if self.jsonData.get('s_d0') is None:
-				continue
-
-			progress.update(perc + 1)
-			sleep(0.1)
-
+		#classify the data and write into database
+		for i in range(self.jsonData.get('num_of_records')):
 			json_body = [{
-				"measurement": self.parseCity.capitalize(),
-				"tags": {
-					"Device_id": self.jsonData.get('device_id')
-				},
-				"time": self.jsonData.get('timestamp'),
-				"fields":{
-					"PM2.5":self.jsonData.get('s_d0'),
-					"s_d1": self.jsonData.get('s_d1'),
-					"s_d2": self.jsonData.get('s_d2'),
-					"Temperature":float(self.jsonData.get('s_t0')),
-					"Humidity": self.jsonData.get('s_h0'),
-					"Gps_lat": float(self.jsonData.get('gps_lat')),
-					"Gps_lon": float(self.jsonData.get('gps_lon')),
-					"Gps_num": self.jsonData.get('gps_num'),
-					"app": self.jsonData.get('app')
+				"time": self.jsonData.get('version'),
+				"fields": {
+					"device_id": self.jsonData.get('feeds')[i].get('device_id'),
+					"PM2.5": self.jsonData.get('feeds')[i].get('s_d0'),
+					"Temperature": float(self.jsonData.get('feeds')[i].get('s_t0')),
+					"Humidity": self.jsonData.get('feeds')[i].get('s_h0'),
+					"Gps_lat": float(self.jsonData.get('feeds')[i].get('gps_lat')),
+					"Gps_lon": float(self.jsonData.get('feeds')[i].get('gps_lon')),
+					"Gps_num": self.jsonData.get('feeds')[i].get('gps_num')
 				}
 			}]
 			self.client.write_points(json_body)
-		progress.finish()
+
+		#parse record
+		self.record()
+
+	def record(self):
+		#connect with DB (AirBox_record / LASS_record)
+		self.database = 'AirBox_record'
+		self.client = InfluxDBClient('localhost', 8086, 'root', 'root', self.database)
+		self.client.create_database(database)
+
+		json_body = [{
+			"time": strftime("%Y-%m-%d_%H:%M:%S", gmtime()),
+			"fields": {
+				"parse_time": self.jsonData.get('version'),
+				"parse_site": self.jsonData.get('num_of_records'),
+				"parse_src": self.jsonData.get('source')
+			}
+		}]
+		self.client.write_points(json_body)
--- airbox_parser/lass_main.py
+++ airbox_parser/lass_main.py
@@ -3,35 +3,20 @@ import sys
 import json
 
 #custom class
-from airbox.querySiteID import querySiteID
 from airbox.parseSite import parseSite
 
 
 def main():
-	cityList = ['taipei', 'newtaipei', 'taichung', 'tainan', 'kaohsiung']
-	option = sys.argv[1]
+	arg_src = sys.argv[1]
+	database = 'AirBox_test'
 
-	if option == '-q':
-		print('>>> Query site ID: All')
-		query = querySiteID(cityList)
-
-		with open('siteID.json', 'w', encoding = 'utf-8-sig') as file:
-			json.dump(query.queryID(), file)
-
-	elif option == '-p':
-		arg_city = sys.argv[2]
-		database = 'AirBox_test'
-
-		with open('siteID.json', encoding = 'utf-8-sig') as file:
-			data = json.load(file)
-
-		#parse site data
-		parse = parseSite(arg_city, data[arg_city], database)
-		while True:
-			start = time()	#debug
-			parse.parseData()
-			print('Execution: ' + str(time() - start) + ' sec')	#debug
-			#sleep(20)	#debug
+	#parse site data
+	parse = parseSite(arg_src, database)
+	while True:
+		start = time()	#debug
+		parse.parseData()
+		print('Execution: ' + str(time() - start) + ' sec')	#debug
+		sleep(300)	#debug
 
 
 if __name__ == '__main__':
