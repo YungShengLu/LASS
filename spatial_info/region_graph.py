eb895791b2bcf8f33054e97a4729ce446aa5dd15
balilarder <balilarder@yahoo.com.tw>
2016-11-29 18:12:14 +0800

    graph class

--- spatial_info/region_graph.py
+++ spatial_info/region_graph.py
@@ -1,62 +1,24 @@
 '''
 * The program will query data in the DB, output a graph with nodes and links.
-
 * Output JSON file?
 
-* example(bigger nodes):
-Taipei: {<id1>, <id2>...}
-Kaohsiung: {<id3>, <id4>}
-
-* example(smaller node, with distance):
-{id1: {id2:13, id3:16, id4:18}}
-{id2: {id1:13, id6:19}}....
-
+* Nodes has different types(lass/airbox/factory)
 '''
-# recursivily find all files
-import os	
+class Nodes(object):
+	def __init__(self, id, type, lon, lat):
+		super(Nodes, self).__init__()
+		self.id = id
+		self.type = type
+		self.lon = lon
+		self.lat = lat
+	def hello():
+		print("I'm "+ id)
+
 
 def main():
-	cyties = []
-	for city in os.listdir("../PM25_analysis_tool/timeFactorObservation"):
-		print(city)
-		cyties.append(city)
-	print("use list to get all GPSlist graph")
-	
-	big_node = {}				
-	small_node = {}	
-	locations = {}	
-
-	# open GPS files in each city's folder
-	for city in cyties:
-		big_node[city] = []
-		GPSs = open("../PM25_analysis_tool/timeFactorObservation/"+city+"/GPSList.csv", 'r')
-		
-		print("print"+city)
-
-		content = GPSs.readlines()
-
-		
-		for data in content:
-			data = data.rstrip()		# remove '\n'
-			print(data)
-			data = data.split(',')		# split by ',' to get all attribute
-			print(data)
-			id = data[0]
-			lon = data[1]				# longitude
-			lat = data[2]				# latitude
-			print(id,lon,lat)
-
-			# add to graph
-			big_node[city].append(id)
-			small_node[id] = {}			# graph: based on distance
-			locations[id] = (lon, lat)
-
-
-		print(len(content))
-		print()
-	print(big_node,len(big_node))
-	print(small_node,len(small_node))
-	print(locations,len(locations))
+	f = open("data.csv", 'r')
+	lines = f.readlines()
+	print(len(lines))
 
 # fuction: compute distance given lon, lat
 def compute_dis(lon, lat):
