44178239f17d78d489a8d9d1c2f732f2a746c3ac
Pin-Xuan She <snakeneedy@gmail.com>
2016-12-22 11:05:23 +0800

    Rename aqiColor->legendColor,add legend and search

--- website/js/map.js
+++ website/js/map.js
@@ -3,54 +3,92 @@ var position = [23.583, 120.583],
     minZoom =  7,
     defaultZoom = 8,
     maxZoom = 18,
-    aqiColor = ['#9CFF9C', '#31FF00', '#31CF00', '#FFFF00', '#FFCF00',
+    legendGrade = [0, 11, 23, 35, 41, 47, 53, 58, 64, 70],
+    legendColor = ['#9CFF9C', '#31FF00', '#31CF00', '#FFFF00', '#FFCF00',
     '#FF9A00', '#FF6464', '#FF0000', '#990000', '#CE30FF'];
-var map;
+var map,
+    layerAirData;
 
-function getAqiColor(aqi) {
-    return aqi < 11 ? aqiColor[0] :
-        aqi < 23 ? aqiColor[1] :
-        aqi < 35 ? aqiColor[2] :
-        aqi < 41 ? aqiColor[3] :
-        aqi < 47 ? aqiColor[4] :
-        aqi < 53 ? aqiColor[5] :
-        aqi < 58 ? aqiColor[6] :
-        aqi < 64 ? aqiColor[7] :
-        aqi < 70 ? aqiColor[8] :
-        aqiColor[9];
+function getLegendColor(aqi) {
+    return aqi < 11 ? legendColor[0] :
+        aqi < 23 ? legendColor[1] :
+        aqi < 35 ? legendColor[2] :
+        aqi < 41 ? legendColor[3] :
+        aqi < 47 ? legendColor[4] :
+        aqi < 53 ? legendColor[5] :
+        aqi < 58 ? legendColor[6] :
+        aqi < 64 ? legendColor[7] :
+        aqi < 70 ? legendColor[8] :
+        legendColor[9];
 }
 
 function renderAirData(data) {
     var opacity = 0.5,
-        radius = 500;
+        radiusCircle = 500,
+        radiusCircleMarker = 1;
     var title,
         color,
         circle,
+        circleMarker,
         strPopup;
 
     var I = data.length;
     for(var i = 0; i < I; i++) {
         title = data[i].device_id;
-        color = getAqiColor(data[i].pollution);
+        color = getLegendColor(data[i].pollution);
 
         circle = L.circle([data[i].lat, data[i].lon], {
             color: color,
             fillColor: color,
             fillOpacity: opacity,
-            radius: radius
+            radius: radiusCircle
         }).addTo(map);
+        circleMarker = new L.CircleMarker([data[i].lat, data[i].lon], {
+            color: color,
+            fillColor: color,
+            fillOpacity: opacity,
+            radius: radiusCircleMarker
+        });
 
         strPopup = "SiteName: " + (i + 1).toString()
             + "<br /> Record timestamp : " + data[i].timestamp
             + "<br/> PM 2.5 : " + data[i].pollution
             + "<br /> ID: " + data[i].device_id;
         circle.bindPopup(strPopup);
+        circleMarker.bindPopup(strPopup);
+        layerAirData.addLayer(circleMarker);
     }
+}
+
+function createLegend() {
+    var legendLabel = [];
+
+    var legend = L.control({
+        position: 'bottomright'
+    });
+    legend.onAdd = function(map) {
+        var div = L.DomUtil.create('div', 'info legend');
+
+        // Generate a label with a colored square for each interval.
+        for (var i = 0; i < legendGrade.length; ++i) {
+            div.innerHTML += legendLabel.push('<i style="background:'
+                    + getLegendColor(legendGrade[i] + 1)
+                    + ';">&nbsp;&nbsp;&nbsp;&nbsp;</i> '
+                    + legendGrade[i]
+                    + (legendGrade[i + 1] ? '&ndash;'+legendGrade[i + 1] : '+')
+                    );
+        }
+        div.innerHTML = legendLabel.join('<br>');
+
+        return div;
+    };
+    return legend;
 }
 
 // init map
 (function() {
-    var airData = [];
+    var airData = [],
+        search;
 
     map = L.map('map').setView(position, defaultZoom);
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -59,6 +97,9 @@ function renderAirData(data) {
             continuousWorld: false,
             attribution: 'Map data &copy; OpenStreetMap contributors'
             }).addTo(map);
+
+    layerAirData = new L.LayerGroup();
+    map.addLayer(layerAirData);
 
     // load csv
     d3.csv('data/csv/lass.csv', function(error, data) {
@@ -76,5 +117,18 @@ function renderAirData(data) {
 
     // add sidebar
     L.control.sidebar('sidebar').addTo(map);
+
+    // add legend
+    createLegend().addTo(map);
+    
+    // add search
+    search = new L.Control.Search({
+        position: 'topright',
+        layer: layerAirData,
+        initial: false,
+        zoom: 12,
+        marker: false
+    });
+    map.addControl(search);
 })();
 
